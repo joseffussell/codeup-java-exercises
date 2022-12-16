474c9437acf664daeb71227923ff4aac528afcde
Josef Fussell <josef.k.fussell@gmail.com>
2022-12-16 15:35:12 -0600

    quicksave

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
-        System.out.println("The value ");
+        System.out.println("The value of pi is approximately " + pi);
+
+        Scanner scanner = new Scanner(System.in);
+//
+//        int giveNumber = scanner.nextInt();
+//        System.out.println(giveNumber);
+//
+//        String firstWord = scanner.next();
+//        String secondWord = scanner.next();
+//        String thirdWord = scanner.next();
+//        System.out.println("Your first word was: " + firstWord + " Your second word was: " + secondWord + " Your third word was: " + thirdWord);
+
+        String enterSentence = scanner.nextLine();
+        System.out.println(enterSentence);
     }
 }
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,33 +6,40 @@ public class HelloWorld {
         int myFavoriteNumber = 34;
         System.out.println(myFavoriteNumber);
 
-//        String myString = "This is a string";
-//        System.out.println(myString);
-
-//        char c = myString.charAt(0);
-//        System.out.println("First letter in myString is: " +c);
-
-        double myString = 3.14159;
+        String myString = "pizza is good";
         System.out.println(myString);
 
-//        long myNumber = 3.14; //data type expected is a double instead of a long
-//        System.out.println(myNumber); //myNumber is not initialized error
-//        long myNumber = 123L;
-//        System.out.println(myNumber); //L does not come through on sys out.
-//        long myNumber = 123;
-//        System.out.println(myNumber); //I feel 123 works because java doesn't have to parse a decimal
-//          float myNumber = 3.14; //incompatible types error
-//        int x = 5;
-//        System.out.println(x++); //increments after code is run
-//        System.out.println(x);
+        double myStringWithPi = 3.14159;
+
+        myString = String.valueOf(myStringWithPi);
+        System.out.println(myString);
+
+//      Long myNumber = 123L;
+//      System.out.println(myNumber);
+
+//      float myNumber = 3.14;
 
 //        int x = 5;
-//        System.out.println(++x); //increments before code is run
-//        System.out.println(x);
+//        System.out.println(x++); // increments after
+//        System.out.println(x); // prints said increment
 
-        String theNumberThree = "three";
-        Object o = theNumberThree;
-        int three = (int) o;
+//        int x = 5;
+//        System.out.println(++x); //prints before execution
+//        System.out.println(x); //re-print
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o; //casting error
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+
+        Scanner scanner = new Scanner(System.in);
+
 
 
 
