30ee6610d22a4bb0ff3f56c764a6dc98a664e761
Josef Fussell <josef.k.fussell@gmail.com>
2022-12-26 12:48:06 -0600

    quicksave

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -2,5 +2,25 @@ package shapes;
 
 public class Circle {
 
+    private double radius;
 
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        System.out.println(Math.PI * (Math.pow(this.radius, 2)));
+        return Math.PI * (Math.pow(this.radius, 2));
+    }
+    public double getCircumference() {
+        System.out.println(2 * Math.PI * this.radius);
+        return 2 * Math.PI * this.radius;
+    }
+
+
+    public static void main(String[] args) {
+        Circle circles = new Circle(4.0);
+        circles.getArea();
+        circles.getCircumference();
+    }
 }
